c61ccec61f6a059cfb07afd141f0046628ed33be
Damon Alexander <51380857+DaemonAlex@users.noreply.github.com>
2024-03-02 02:23:53 -0600

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,6 @@
+Config = {}
+Config.PoliceVehicles = {
+    '11caprice', 'tahoe', '03expeditionr', 'newvic', '16taurus', '16explorer', '19durango', '18tahoe', '18charger', '14charger'
+}
+Config.SkinsRange = {1, 10}
+Config.ExtrasRange = {1, 20}
