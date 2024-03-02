c1e235312d6d6cbac4d4035237aab9798d64cec3
Damon Alexander <51380857+DaemonAlex@users.noreply.github.com>
2024-03-02 02:23:18 -0600

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'DaemonAlex (schtoop)'
+description 'Emergency Vehicle Modification System'
+version '1.0.0'
+
+shared_script 'config.lua'
+client_script 'client.lua'
+server_script 'server.lua'
+
+dependency 'ox_lib' -- Ensure OX Menu and OX Lib are dependencies
