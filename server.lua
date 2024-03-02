cec1a78d499bad339ee3363a8e45b603acb6986d
Damon Alexander <51380857+DaemonAlex@users.noreply.github.com>
2024-03-02 12:54:57 -0600

    Update server.lua
    add logging and optimization

--- server.lua
+++ server.lua
@@ -4,9 +4,17 @@ RegisterNetEvent('vehiclemods:server:verifyPoliceJob')
 AddEventHandler('vehiclemods:server:verifyPoliceJob', function()
     local src = source
     local Player = QBCore.Functions.GetPlayer(src)
-    if Player and Player.PlayerData.job.name == 'police' then
-        TriggerClientEvent('vehiclemods:client:openVehicleModMenu', src)
+    
+    if Player then
+        if Player.PlayerData.job and Player.PlayerData.job.name == 'police' then
+            TriggerClientEvent('vehiclemods:client:openVehicleModMenu', src)
+            print("Vehicle mod menu opened for player ID: " .. src)
+        else
+            TriggerClientEvent('QBCore:Notify', src, 'This menu is only for Emergency vehicles.', 'error')
+            print("Unauthorized vehicle mod menu access attempt by player ID: " .. src)
+        end
     else
-        TriggerClientEvent('QBCore:Notify', src, 'This menu is only for Emergency vehicles.', 'error')
+        print("Failed to retrieve player object for source ID: " .. src)
     end
 end)
+
