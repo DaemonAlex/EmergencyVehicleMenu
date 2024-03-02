629adc317a09a8cde0dfae6a53e92a019683260e
Damon Alexander <51380857+DaemonAlex@users.noreply.github.com>
2024-03-02 12:47:13 -0600

    Update client.lua

--- client.lua
+++ client.lua
@@ -7,29 +7,17 @@ RegisterCommand('modvehicle', function()
         if GetVehicleClass(vehicle) == 18 then
             TriggerServerEvent('vehiclemods:server:verifyPoliceJob')
         else
-            exports['ox_lib']:textUI('This menu is only for Emergency vehicles.', { duration = 5000, position = 'top-center', type = 'error' })
+            if exports['ox_lib'] then
+                exports['ox_lib']:textUI('This menu is only for Emergency vehicles.', { duration = 5000, position = 'top-center', type = 'error' })
+            else
+                print("ox_lib is not available.")
+            end
         end
     else
-        exports['ox_lib']:textUI('You must be in a vehicle to use this command.', { duration = 5000, position = 'top-center', type = 'error' })
+        if exports['ox_lib'] then
+            exports['ox_lib']:textUI('You must be in a vehicle to use this command.', { duration = 5000, position = 'top-center', type = 'error' })
+        else
+            print("ox_lib is not available.")
+        end
     end
 end, false)
-
-RegisterNetEvent('vehiclemods:client:openVehicleModMenu')
-AddEventHandler('vehiclemods:client:openVehicleModMenu', function()
-    local vehicle = GetVehiclePedIsIn(PlayerPedId(), false)
-    openVehicleModMenu(vehicle)
-end)
-
-function openVehicleModMenu(vehicle)
-    -- Apply performance mods to level 4 automatically
-    local performanceMods = {11, 12, 13, 15, 16} -- Mod types for performance
-    for _, modType in ipairs(performanceMods) do
-        SetVehicleMod(vehicle, modType, 3, false) -- Mods are 0-indexed
-    end
-    exports['ox_lib']:textUI('All performance mods upgraded to level 4.', { duration = 5000, position = 'top-center', type = 'success' })
-
-    -- Further implementation for opening the OX Menu for skins and extras
-end
-
--- Placeholder for `openSkinSubMenu` and `openExtrasSubMenu` functions
-
