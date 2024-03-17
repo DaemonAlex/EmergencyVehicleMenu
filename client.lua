3249f67425e6baaa8836eb70af4abc21a83cc539
Damon Alexander <51380857+DaemonAlex@users.noreply.github.com>
2024-03-16 20:11:42 -0500

    Update client.lua

--- client.lua
+++ client.lua
@@ -8,16 +8,5 @@ RegisterCommand('modvehicle', function()
             TriggerServerEvent('vehiclemods:server:verifyPoliceJob')
         else
             if exports['ox_lib'] then
-                exports['ox_lib']:textUI('This menu is only for Emergency vehicles.', { duration = 5000, position = 'top-center', type = 'error' })
-            else
-                print("ox_lib is not available.")
-            end
-        end
-    else
-        if exports['ox_lib'] then
-            exports['ox_lib']:textUI('You must be in a vehicle to use this command.', { duration = 5000, position = 'top-center', type = 'error' })
-        else
-            print("ox_lib is not available.")
-        end
-    end
-end, false)
+                exports['ox_lib']:textUI('This menu is only for Emergency vehicles.', { duration = 5000, position =
+...
