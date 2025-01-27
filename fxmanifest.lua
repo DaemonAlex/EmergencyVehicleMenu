15a5412cbf2ae17ae75b394826b24575f6d25151
Damon Alexander <51380857+DaemonAlex@users.noreply.github.com>
2025-01-27 16:43:53 -0600

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,12 +1,15 @@
 fx_version 'cerulean'
 games { 'gta5' }
 
-author 'DaemonAlex (schtoop)'
-description 'Emergency Vehicle Modification System'
+author 'Your Name'
+description 'Police Vehicle Modification System'
 version '1.0.0'
 
 shared_script 'config.lua'
 client_script 'client.lua'
 server_script 'server.lua'
 
-dependency 'ox_lib' -- Ensure OX Menu and OX Lib are dependencies
+dependencies {
+    'qb-core',
+    'ox_lib' -- Ensure ox_lib is declared as a dependency
+}
