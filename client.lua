851aa47dab533af90c761a00d474394581160c9a
Damon Alexander <51380857+DaemonAlex@users.noreply.github.com>
2025-01-27 15:28:52 -0600

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,12 +1,76 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 
+-- Command to open the vehicle modification menu
 RegisterCommand('modvehicle', function()
     local ped = PlayerPedId()
     if IsPedSittingInAnyVehicle(ped) then
         local vehicle = GetVehiclePedIsIn(ped, false)
-        if GetVehicleClass(vehicle) == 18 then
+        local vehicleModel = GetEntityModel(vehicle)
+        local vehicleName = GetDisplayNameFromVehicleModel(vehicleModel):lower()
+
+        -- Check if the vehicle is in the Config.PoliceVehicles list
+        if IsVehicleInConfig(vehicleName) then
             TriggerServerEvent('vehiclemods:server:verifyPoliceJob')
         else
-            if exports['ox_lib'] then
-                exports['ox_lib']:textUI('This menu is only for Emergency vehicles.', { duration = 5000, position =
-...
+            exports.ox_lib:notify({
+                title = 'Error',
+                description = 'This menu is only for approved police vehicles.',
+                type = 'error',
+                duration = 5000
+            })
+        end
+    else
+        exports.ox_lib:notify({
+            title = 'Error',
+            description = 'You must be inside a vehicle to use this command.',
+            type = 'error',
+            duration = 5000
+        })
+    end
+end)
+
+-- Function to check if the vehicle is in the Config.PoliceVehicles list
+function IsVehicleInConfig(vehicleName)
+    for _, v in pairs(Config.PoliceVehicles) do
+        if v == vehicleName then
+            return true
+        end
+    end
+    return false
+end
+
+-- Event to open the vehicle modification menu
+RegisterNetEvent('vehiclemods:client:openVehicleModMenu')
+AddEventHandler('vehiclemods:client:openVehicleModMenu', function()
+    OpenVehicleModMenu()
+end)
+
+-- Function to open the modification menu using ox_lib
+function OpenVehicleModMenu()
+    local options = {
+        {
+            title = 'Performance Upgrades',
+            description = 'Upgrade your vehicle\'s performance.',
+            event = 'vehiclemods:client:upgradePerformance',
+            args = {}
+        },
+        {
+            title = 'Change Skin',
+            description = 'Change your vehicle\'s appearance.',
+            event = 'vehiclemods:client:changeSkin',
+            args = {}
+        },
+        {
+            title = 'Toggle Extras',
+            description = 'Enable or disable vehicle extras.',
+            event = 'vehiclemods:client:toggleExtras',
+            args = {}
+        }
+    }
+
+    exports.ox_lib:showContext('vehicleModMenu', {
+        id = 'vehicleModMenu',
+        title = 'Vehicle Modification Menu',
+        options = options
+    })
+end
