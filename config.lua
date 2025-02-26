2f6bf4d974ce954d59de7309dc63375ed3e6c8a4
Damon Alexander <51380857+DaemonAlex@users.noreply.github.com>
2025-02-25 22:31:06 -0600

    Update config.lua

--- config.lua
+++ config.lua
@@ -3,8 +3,7 @@ Config = {}
 -- Select the framework you're using: 'qb-core', 'qbc-core', 'esx', or 'standalone'
 Config.Framework = 'qb-core'  -- Change this value based on your server
 
--- Access permissions for jobs
 Config.JobAccess = {
-    ['police'] = true,  -- Can be modified to add other jobs
+    ['police'] = true,  
     ['ambulance'] = true
 }
