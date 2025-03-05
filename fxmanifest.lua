315567e5aa25e245b60f07b328d7bd38c02cb3c3
DaemonScripts <51380857+DaemonAlex@users.noreply.github.com>
2025-03-05 13:00:24 -0600

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -8,9 +8,9 @@ version '1.0.0'
 lua54 'yes'
 
 shared_script 'config.lua'
+shared_script '@qb-core/shared/locale.lua'
 client_script 'client.lua'
 server_script 'server.lua'
-shared_script '@qb-core/shared/locale.lua'
 
 dependencies {
     -- Uncomment the framework you are using. Leave all uncommented if you are STANDALONE
