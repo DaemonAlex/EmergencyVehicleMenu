2ee59c9c790fe9f958dc90a5bcbd875b01d3c6af
DaemonScripts <51380857+DaemonAlex@users.noreply.github.com>
2025-04-20 20:07:11 -0500

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,45 +1,29 @@
-RegisterCommand('modveh', function()
-    local job = ''
-    local QBCore = exports['qb-core']:GetCoreObject()
-        
-if Config.Framework == 'qb-core' or Config.Framework == 'qbx-core' then
-    if QBCore and QBCore.Functions then
-        local playerData = QBCore.Functions.GetPlayerData()
-        job = playerData and playerData.job and playerData.job.name or 'unknown'
-    else
-        print("^1ERROR:^0 QBCore is not defined, please make sure the qb-core resource is started.")
-        job = 'unknown'
-    end
+RegisterCommand(Config.Command, function()
+    -- In standalone mode, we don't need to check for job permissions
+    -- This will directly open the menu for any player
+    print("^2SUCCESS:^0 Opening vehicle modification menu...")
+    OpenVehicleModMenu()
+end, false)
 
-    elseif Config.Framework == 'esx' then
-        local playerData = ESX.GetPlayerData()
-        job = playerData and playerData.job and playerData.job.name or 'unknown'
-    elseif Config.Framework == 'standalone' then
-        job = 'standalone'
-    end
-
-    if Config.Framework ~= 'standalone' and not Config.JobAccess[job] then
-        print("^1ERROR:^0 Access denied for job: " .. job)
-
-        if Config.Framework == 'qb-core' or Config.Framework == 'qbc-core' then
-            TriggerEvent('ox_lib:notify', {title = 'Access Denied', description = 'You must be a first responder to use this.', type = 'error'})
-        elseif Config.Framework == 'esx' then
-            ESX.ShowNotification('You must be a first responder to use this.')
-        end
-
+-- Main menu function
+function OpenVehicleModMenu()
+    -- Check if player is in a vehicle
+    local playerPed = PlayerPedId()
+    if not IsPedInAnyVehicle(playerPed, false) then
+        lib.notify({
+            title = 'Error',
+            description = 'You must be in a vehicle to use this menu.',
+            type = 'error',
+            duration = 5000
+        })
         return
     end
 
-    print("^2SUCCESS:^0 Access granted. Opening menu...")
-    TriggerEvent('vehiclemods:client:openVehicleModMenu')
-end, false)
-
-RegisterNetEvent('vehiclemods:client:openVehicleModMenu')
-AddEventHandler('vehiclemods:client:openVehicleModMenu', function()
     local options = {
         {
             title = 'Liveries',
             description = 'Select a vehicle livery.',
+            icon = 'paint-roller',
             onSelect = function()
                 OpenLiveryMenu()
             end
@@ -47,6 +31,7 @@ AddEventHandler('vehiclemods:client:openVehicleModMenu', function()
         {
             title = 'Extras',
             description = 'Enable or disable vehicle extras.',
+            icon = 'puzzle-piece',
             onSelect = function()
                 OpenExtrasMenu()
             end
@@ -54,118 +39,306 @@ AddEventHandler('vehiclemods:client:openVehicleModMenu', function()
         {
             title = 'Doors',
             description = 'Open or close individual doors.',
+            icon = 'door-open',
             onSelect = function()
                 OpenDoorsMenu()
+            end
+        },
+        {
+            title = 'Save Configuration',
+            description = 'Save the current vehicle configuration.',
+            icon = 'save',
+            onSelect = function()
+                SaveVehicleConfiguration()
             end
         }
     }
 
     lib.registerContext({
-        id = 'EmergencyVehicleMenu',
-        title = 'Emergency Vehicle Menu',
+        id = 'VehicleModMenu',
+        title = 'Vehicle Modification Menu',
         options = options,
-        close = false
+        close = true
     })
-    lib.showContext('EmergencyVehicleMenu')
-end)
+    lib.showContext('VehicleModMenu')
+end
 
+-- Livery menu function
 function OpenLiveryMenu()
     local vehicle = GetVehiclePedIsIn(PlayerPedId(), false)
     local options = {}
     local numLiveries = GetVehicleLiveryCount(vehicle)
     
+    if numLiveries <= 0 then
+        lib.notify({
+            title = 'No Liveries',
+            description = 'This vehicle has no liveries available.',
+            type = 'inform',
+            duration = 5000
+        })
+        return OpenVehicleModMenu()
+    end
+    
+    -- Add "Default" option at the top
+    table.insert(options, {
+        title = 'Default Livery',
+        icon = 'ban',
+        onSelect = function()
+            SetVehicleLivery(vehicle, 0)
+            lib.notify({
+                title = 'Livery Applied',
+                description = 'Applied Default Livery.',
+                type = 'success',
+                duration = 3000
+            })
+        end
+    })
+    
     for i = 0, numLiveries - 1 do
         table.insert(options, {
             title = 'Livery ' .. i,
+            icon = 'palette',
             onSelect = function()
                 SetVehicleLivery(vehicle, i)
                 lib.notify({
                     title = 'Livery Applied',
                     description = 'Applied Livery ' .. i .. '.',
                     type = 'success',
-                    duration = 5000
+                    duration = 3000
                 })
-                OpenLiveryMenu()
             end
         })
     end
 
     lib.registerContext({
         id = 'LiveryMenu',
-        title = 'Select Livery',
+        title = 'Select Vehicle Livery',
         options = options,
-        menu = 'EmergencyVehicleMenu',
-        close = false
+        menu = 'VehicleModMenu',
+        close = true
     })
     lib.showContext('LiveryMenu')
 end
 
+-- Extras menu function
 function OpenExtrasMenu()
     local vehicle = GetVehiclePedIsIn(PlayerPedId(), false)
     local options = {}
+    local hasExtras = false
     
-    for i = 1, 20 do
+    for i = 0, 20 do
         if DoesExtraExist(vehicle, i) then
+            hasExtras = true
+            local state = IsVehicleExtraTurnedOn(vehicle, i)
             table.insert(options, {
-                title = 'Toggle Extra ' .. i,
+                title = 'Extra ' .. i,
+                icon = state and 'toggle-on' or 'toggle-off',
+                description = state and 'Currently: Enabled' or 'Currently: Disabled',
                 onSelect = function()
-                    local state = IsVehicleExtraTurnedOn(vehicle, i)
-                    SetVehicleExtra(vehicle, i, state and 1 or 0)
+                    local newState = not IsVehicleExtraTurnedOn(vehicle, i)
+                    SetVehicleExtra(vehicle, i, not newState)
                     lib.notify({
-                        title = 'Success',
-                        description = 'Toggled Extra ' .. i .. '.',
+                        title = 'Extra Toggled',
+                        description = (newState and 'Enabled' or 'Disabled') .. ' Extra ' .. i .. '.',
                         type = 'success',
-                        duration = 5000
+                        duration = 3000
                     })
-                    OpenExtrasMenu()
+                    OpenExtrasMenu() -- Refresh the menu to update toggle states
                 end
             })
         end
     end
     
+    if not hasExtras then
+        lib.notify({
+            title = 'No Extras',
+            description = 'This vehicle has no extras available.',
+            type = 'inform',
+            duration = 5000
+        })
+        return OpenVehicleModMenu()
+    end
+    
     lib.registerContext({
         id = 'ExtrasMenu',
-        title = 'Toggle Extras',
+        title = 'Vehicle Extras',
         options = options,
-        menu = 'EmergencyVehicleMenu',
-        close = false
+        menu = 'VehicleModMenu',
+        close = true
     })
     lib.showContext('ExtrasMenu')
 end
 
+-- Doors control menu
 function OpenDoorsMenu()
     local vehicle = GetVehiclePedIsIn(PlayerPedId(), false)
     local doors = {
-        { title = 'Driver Door', index = 0 },
-        { title = 'Passenger Door', index = 1 },
-        { title = 'Rear Driver Door', index = 2 },
-        { title = 'Rear Passenger Door', index = 3 },
-        { title = 'Hood', index = 4 },
-        { title = 'Trunk', index = 5 }
+        { title = 'Driver Door', index = 0, icon = 'door-open' },
+        { title = 'Passenger Door', index = 1, icon = 'door-open' },
+        { title = 'Rear Left Door', index = 2, icon = 'door-open' },
+        { title = 'Rear Right Door', index = 3, icon = 'door-open' },
+        { title = 'Hood', index = 4, icon = 'car' },
+        { title = 'Trunk', index = 5, icon = 'box' }
     }
 
     local options = {}
     for _, door in pairs(doors) do
+        local isOpen = GetVehicleDoorAngleRatio(vehicle, door.index) > 0
         table.insert(options, {
-            title = 'Toggle ' .. door.title,
+            title = door.title,
+            icon = door.icon,
+            description = isOpen and 'Currently: Open' or 'Currently: Closed',
             onSelect = function()
-                if GetVehicleDoorAngleRatio(vehicle, door.index) > 0 then
+                if isOpen then
                     SetVehicleDoorShut(vehicle, door.index, false)
+                    lib.notify({
+                        title = 'Door Control',
+                        description = 'Closed ' .. door.title .. '.',
+                        type = 'success',
+                        duration = 3000
+                    })
                 else
                     SetVehicleDoorOpen(vehicle, door.index, false, false)
+                    lib.notify({
+                        title = 'Door Control',
+                        description = 'Opened ' .. door.title .. '.',
+                        type = 'success',
+                        duration = 3000
+                    })
                 end
-                OpenDoorsMenu()
+                OpenDoorsMenu() -- Refresh the menu to update door states
             end
         })
     end
+    
+    -- Add "All Doors" options
+    table.insert(options, {
+        title = 'Open All Doors',
+        icon = 'door-open',
+        description = 'Open all doors at once.',
+        onSelect = function()
+            for i = 0, 5 do
+                SetVehicleDoorOpen(vehicle, i, false, false)
+            end
+            lib.notify({
+                title = 'Door Control',
+                description = 'All doors opened.',
+                type = 'success',
+                duration = 3000
+            })
+            OpenDoorsMenu()
+        end
+    })
+    
+    table.insert(options, {
+        title = 'Close All Doors',
+        icon = 'door-closed',
+        description = 'Close all doors at once.',
+        onSelect = function()
+            for i = 0, 5 do
+                SetVehicleDoorShut(vehicle, i, false)
+            end
+            lib.notify({
+                title = 'Door Control',
+                description = 'All doors closed.',
+                type = 'success',
+                duration = 3000
+            })
+            OpenDoorsMenu()
+        end
+    })
 
     lib.registerContext({
         id = 'DoorsMenu',
-        title = 'Doors Control',
+        title = 'Door Controls',
         options = options,
-        menu = 'EmergencyVehicleMenu',
-        close = false
+        menu = 'VehicleModMenu',
+        close = true
     })
     lib.showContext('DoorsMenu')
 end
 
+-- Function to save vehicle configuration
+function SaveVehicleConfiguration()
+    local vehicle = GetVehiclePedIsIn(PlayerPedId(), false)
+    if not vehicle or vehicle == 0 then
+        lib.notify({
+            title = 'Error',
+            description = 'You must be in a vehicle to save configuration.',
+            type = 'error',
+            duration = 5000
+        })
+        return
+    end
+    
+    local vehicleModel = GetEntityModel(vehicle)
+    local livery = GetVehicleLivery(vehicle)
+    
+    -- Collect extras configuration
+    local extras = {}
+    for i = 0, 20 do
+        if DoesExtraExist(vehicle, i) then
+            extras[i] = IsVehicleExtraTurnedOn(vehicle, i)
+        end
+    end
+    
+    -- Convert extras to JSON string
+    local extrasJson = json.encode(extras)
+    
+    -- Request server to save configuration
+    TriggerServerEvent('vehiclemods:server:saveModifications', vehicleModel, livery, extrasJson)
+    
+    lib.notify({
+        title = 'Configuration Saved',
+        description = 'Your vehicle configuration has been saved.',
+        type = 'success',
+        duration = 5000
+    })
+end
+
+-- Event to apply saved modifications
+RegisterNetEvent('vehiclemods:client:returnModifications')
+AddEventHandler('vehiclemods:client:returnModifications', function(data)
+    if not data then return end
+    
+    local vehicle = GetVehiclePedIsIn(PlayerPedId(), false)
+    if not vehicle or vehicle == 0 then return end
+    
+    -- Apply saved livery if available
+    if data.skin then
+        SetVehicleLivery(vehicle, tonumber(data.skin))
+    end
+    
+    -- Apply saved extras if available
+    if data.extras then
+        local extras = json.decode(data.extras)
+        for extraId, enabled in pairs(extras) do
+            SetVehicleExtra(vehicle, tonumber(extraId), not enabled)
+        end
+    end
+    
+    lib.notify({
+        title = 'Configuration Applied',
+        description = 'Your saved vehicle configuration has been applied.',
+        type = 'success',
+        duration = 5000
+    })
+end)
+
+-- Load saved configuration when entering a vehicle
+AddEventHandler('gameEventTriggered', function(name, args)
+    if name == 'CEventNetworkPlayerEnteredVehicle' then
+        local playerServerId = args[1]
+        local vehicle = args[2]
+        
+        -- Check if it's the local player
+        if GetPlayerServerId(PlayerId()) == playerServerId then
+            -- Wait a moment for the vehicle to fully load
+            Wait(500)
+            
+            -- Request saved modifications for this vehicle model
+            local vehicleModel = GetEntityModel(vehicle)
+            TriggerServerEvent('vehiclemods:server:getModifications', vehicleModel)
+        end
+    end
+end)
