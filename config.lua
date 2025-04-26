bdc5e5a3b1936e71ee6db5084e97b539058daadb
DaemonScripts <51380857+DaemonAlex@users.noreply.github.com>
2025-04-26 17:37:35 -0500

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,9 +1,9 @@
 Config = Config or {}
 
--- Framework options: 'qb-core', 'qbc-core', 'esx', 'standalone'
+-- Framework options: 'qb-core', 'qbx-core', 'esx', 'standalone'
 Config.Framework = 'standalone'
 
-Config.Debug = false
+Config.Debug = true -- Set to true to get more debug information
 
 Config.JobAccess = {
     -- Original jobs
@@ -28,6 +28,27 @@ Config.JobAccess = {
 }
 
 Config.CustomLiveries = Config.CustomLiveries or {}
+
+-- Vehicle resource directories configuration
+Config.VehicleResourceDirs = {
+    ["police"] = "resources/[vehicles]/[police]",
+    ["police2"] = "resources/[vehicles]/[police]",
+    ["police3"] = "resources/[vehicles]/[police]",
+    ["police4"] = "resources/[vehicles]/[police]",
+    ["policeb"] = "resources/[vehicles]/[police]",
+    ["sheriff"] = "resources/[vehicles]/[police]",
+    ["sheriff2"] = "resources/[vehicles]/[police]",
+    ["fbi"] = "resources/[vehicles]/[police]",
+    ["fbi2"] = "resources/[vehicles]/[police]",
+    ["pranger"] = "resources/[vehicles]/[police]",
+    
+    ["ambulance"] = "resources/[vehicles]/[FireEMS]",
+    ["firetruk"] = "resources/[vehicles]/[FireEMS]"
+    -- Add more mappings as needed
+}
+
+-- Default directory to use if a vehicle isn't in the mapping
+Config.DefaultVehicleResourceDir = "resources/[vehicles]/[police]"
 
 -- Examples with the correct folder structure
 -- Format: Config.CustomLiveries[vehicle_model] = { {name = "Livery Name", file = "liveries/filename.yft"}, ... }
@@ -74,37 +95,77 @@ Config.VehicleModels = {
 Config.MaxModSlots = 40  -- Maximum number of mod slots supported
 Config.MaxLiverySlots = 20  -- Maximum number of livery slots supported
 
--- Folder structure configuration 
+-- Updated folder structure configuration to handle multiple possibilities
 Config.FolderStructure = {
     BasePath = "stream",
     Liveries = "liveries",
     Models = "model",
-    ModParts = "modparts"
+    ModParts = "modparts",
+    -- Add possible paths for meta files
+    MetaPaths = {
+        "",            -- Direct in vehicle folder
+        "data",        -- In data subfolder
+        "data/%s"      -- In data/vehiclename subfolder (will be formatted with vehicle name)
+    }
 }
 
+-- Function to get resource directory for a vehicle model
+function Config.GetVehicleResourceDir(vehicleModel)
+    local modelName = string.lower(vehicleModel)
+    return Config.VehicleResourceDirs[modelName] or Config.DefaultVehicleResourceDir
+end
+
+-- Updated path finding functions
 function Config.GetLiveryPath(vehicleModel, liveryFile)
+    local resourceDir = Config.GetVehicleResourceDir(vehicleModel)
+    
     if string.match(liveryFile, "^" .. Config.FolderStructure.Liveries .. "/") then
-        return Config.FolderStructure.BasePath .. "/" .. vehicleModel .. "/" .. liveryFile
+        return resourceDir .. "/" .. vehicleModel .. "/" .. liveryFile
     end
     
-    return Config.FolderStructure.BasePath .. "/" .. vehicleModel .. "/" .. Config.FolderStructure.Liveries .. "/" .. liveryFile
+    return resourceDir .. "/" .. vehicleModel .. "/" .. Config.FolderStructure.Liveries .. "/" .. liveryFile
 end
 
 function Config.GetModelPath(vehicleModel, modelFile)
+    local resourceDir = Config.GetVehicleResourceDir(vehicleModel)
+    
     if string.match(modelFile, "^" .. Config.FolderStructure.Models .. "/") then
-        return Config.FolderStructure.BasePath .. "/" .. vehicleModel .. "/" .. modelFile
+        return resourceDir .. "/" .. vehicleModel .. "/" .. modelFile
     end
     
-    return Config.FolderStructure.BasePath .. "/" .. vehicleModel .. "/" .. Config.FolderStructure.Models .. "/" .. modelFile
+    return resourceDir .. "/" .. vehicleModel .. "/" .. Config.FolderStructure.Models .. "/" .. modelFile
 end
 
 function Config.GetModPartPath(vehicleModel, partFile)
+    local resourceDir = Config.GetVehicleResourceDir(vehicleModel)
+    
     -- If the path already includes the modparts folder, use as is
     if string.match(partFile, "^" .. Config.FolderStructure.ModParts .. "/") then
-        return Config.FolderStructure.BasePath .. "/" .. vehicleModel .. "/" .. partFile
+        return resourceDir .. "/" .. vehicleModel .. "/" .. partFile
     end
     
-    return Config.FolderStructure.BasePath .. "/" .. vehicleModel .. "/" .. Config.FolderStructure.ModParts .. "/" .. partFile
+    return resourceDir .. "/" .. vehicleModel .. "/" .. Config.FolderStructure.ModParts .. "/" .. partFile
+end
+
+-- Function to find meta files in various possible locations
+function Config.GetMetaFilePath(vehicleModel)
+    local resourceDir = Config.GetVehicleResourceDir(vehicleModel)
+    local possiblePaths = {}
+    
+    for _, pathTemplate in ipairs(Config.FolderStructure.MetaPaths) do
+        local path
+        if pathTemplate == "" then
+            path = resourceDir .. "/" .. vehicleModel
+        elseif string.find(pathTemplate, "%%s") then
+            path = resourceDir .. "/" .. vehicleModel .. "/" .. string.format(pathTemplate, vehicleModel)
+        else
+            path = resourceDir .. "/" .. vehicleModel .. "/" .. pathTemplate
+        end
+        
+        table.insert(possiblePaths, path)
+    end
+    
+    return possiblePaths
 end
 
 -- add your emergency vehicle models
@@ -125,4 +186,15 @@ Config.EmergencyVehicleModels = {
 
 function Config.IsJobAllowed(job)
     return Config.JobAccess[job] or false
+end
+
+-- Debug function to print paths for a vehicle model
+function Config.DebugPaths(vehicleModel)
+    if Config.Debug then
+        print("^3DEBUG:^0 Vehicle resource directory for " .. vehicleModel .. ": " .. Config.GetVehicleResourceDir(vehicleModel))
+        print("^3DEBUG:^0 Possible meta file paths for " .. vehicleModel .. ":")
+        for i, path in ipairs(Config.GetMetaFilePath(vehicleModel)) do
+            print("  " .. i .. ": " .. path)
+        end
+    end
 end
